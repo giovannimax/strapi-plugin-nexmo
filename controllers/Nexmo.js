e7184bcd768eda9755ced0983775405de6b72a14
giovannimax <mindlesh13@gmail.com>
2019-07-31 17:00:32 +0800

    add nexmo sms services

--- controllers/Nexmo.js
+++ controllers/Nexmo.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * Nexmo.js controller
  *
@@ -16,10 +17,20 @@ module.exports = {
 
   index: async (ctx) => {
     // Add your own logic here.
+    let config = strapi.config.nexmo;
+    let sender = config.sender;
+    let recipient = config.recipient;
+    let order = await strapi.models.orders.findOne({ _id: ctx.params.id});
+  
+    let sms = await strapi.plugins.nexmo.services.nexmo.sendSms(sender, recipient, order);
+
+    if(sms === 'error') {
+      return ctx.send('error');
+    }
 
     // Send 200 `ok`
     ctx.send({
-      message: 'ok'
+      message: sms
     });
   }
 };
--- services/Nexmo.js
+++ services/Nexmo.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Nexmo = require('nexmo');
+
 /**
  * Nexmo.js service
  *
@@ -7,5 +9,42 @@
  */
 
 module.exports = {
+
+    sendSms: async (sender, recipient, order) => {
+            let config = strapi.config.nexmo;
+            let nexmo = new Nexmo(config);
+            
+            let orderType = order.meta.type === "pick-up" ? "PU" : "DEL";
+
+            let orderPlace = order.meta.type === "pick-up" ? order.meta.branch : order.meta.address;
+
+            let payment = order.meta.payment === "paypal" ? "Paid via Paypal" : "COD";
+
+            let message = `New order from ${order.user.name} (${order.meta.phone}). ${orderType} ${order.meta.date} ${order.meta.time} 
+            at ${orderPlace}. ${payment}`;
+
+            var sendSms = new Promise(function(resolve, reject) {
+            nexmo.message.sendSms(sender, recipient, message, (error, response) => {
+            if (error) {
+                // throw error;
+                return reject(error);
+            } else {
+                // console.log("Create Payment Response");
+                // console.log(payment);
+                return resolve(response);
+            }
+            });
+        });
+
+        var sms = null;
+        try {
+            sms = await sendSms;
+        } catch(err) {
+            console.log(err);
+            return 'error';
+        }
+
+        return sms;
+    }
 
 };
